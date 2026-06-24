bd2613eeaa5b378d9bbaf2edd3f03066dee6b47c
student-soumyajit <soumyajitmalakar@gmail.com>
2026-06-24 07:32:28 +0530

    fixed problems

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,14 +36,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../client/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-
-  app.get("/*",(req,res)=>{
-    app.sendFile(path.join(__dirname,"../client","dist","index.html"))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../client", "dist", "index.html")
+    );
+  });
 }
-
 server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   await connectDB();
